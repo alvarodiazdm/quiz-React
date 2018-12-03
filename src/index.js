6b0c2127ecd8197bfa7392236e8ace760f0c78a5
Alvaro Diaz del Mazo <alvarodiazdm@gmail.com>
2018-12-03 11:50:12 +0100

    Paso 3 - Primer Componente

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import ReduxProvider from './redux/ReduxProvider';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<ReduxProvider />, document.getElementById('root'));
 
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 
 function score(state = 0, action = {}){
     switch(action.type){
