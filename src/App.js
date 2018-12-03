f6f210b86d16a6653659f5f54e6585300124c41d
Alvaro Diaz del Mazo <alvarodiazdm@gmail.com>
2018-12-03 12:19:53 +0100

    Paso 4 - Accion de escribir respuesta

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 
 import { connect } from 'react-redux';
 import Game from "./Game";
+import {questionAnswer} from "./redux/actions";
 
 class App extends Component {
   render() {
+      console.log(this.props)
     return (
         <div>
-            <Game question = {this.props.questions[this.props.currentQuestion]}/>
+            <Game question = {this.props.questions[this.props.currentQuestion]}
+                  onQuestionAnswer = {(answer) => {
+                      this.props.dispatch(questionAnswer(this.props.currentQuestion, answer))
+                  }} />
         </div>
     );
   }
